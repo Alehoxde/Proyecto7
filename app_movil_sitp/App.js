45f7afb11fd2787456747395011bdc56790c69c1
Alehandro Muñoz <alehxan@gmail.com>
2026-04-22 08:06:42 -0500

    hola mundo

--- app_movil_sitp/App.js
+++ app_movil_sitp/App.js
@@ -1,27 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
 
 export default function App() {
-  const [message, setMessage] = useState('Cargando...');
-
-  useEffect(() => {
-   fetch('http://192.168.1.87:3000')  // Reemplaza con tu IP real  // Para emulador Android
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-      .catch(err => setMessage('Error: ' + err.message));
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.center}>
-        <Text style={styles.text}>{message}</Text>
-      </View>
+      <Text style={styles.text}>Hola Mundo</Text>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
-  center: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  text: { fontSize: 24 },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+  },
 });
